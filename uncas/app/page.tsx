00a1cc9a15b03d1c46ebe8b6aaf7843cbe6bacc5
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-02-12 16:47:57 -0500

    Using bg-image cover center

--- uncas/app/page.tsx
+++ uncas/app/page.tsx
@@ -1,19 +1,22 @@
-import Image from "next/image";
-import manifest from '@/assets/manifest-1.png';
+//import Image from "next/image";
+//import manifest from '@/assets/manifest-1.png';
+//import { Accordion } from "@/components/ui/accordion";
+
 
 export default function Home() {
   return (
     <section className="h-screen w-full my-8 flex justify-center items-center">
-      <div className="border border-red-500 h-full w-full m-8">    
-        <Image
+      <div className="bg-[url(@/assets/manifest-1.png)] bg-cover bg-center h-full w-full m-8">
+        
+        {/* <Image
           className="object-cover"
           src={manifest}
           alt="manifest page 1"
-        />
+        /> */}
       </div>
     </section>
   );
-}
+};
 
 //  <Image
 //           className="h-[200px] w-[300px] object-contain border border-base-300/50 hover:border-neutral-800 rounded-lg"
