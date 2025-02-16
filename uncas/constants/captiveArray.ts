5338c0f7d71fa0934237953b6552476e12472295
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-02-16 11:48:01 -0500

    Completed transported humans array

--- uncas/constants/captiveArray.ts
+++ uncas/constants/captiveArray.ts
@@ -66,58 +66,30 @@ export const transportedHumans = [
   {name: 'Charles Surrey', age: 15, feet: '4\'', inches: '11 1/2"', colour: 'Black'},
   {name: 'Fredericks Surrey', age: 13, feet: '4\'', inches: '4 1/2"', colour: 'Black'},
   {name: 'Margaret Ann Surrey', age: 9, feet: '3\'', inches: '6 1/4"', colour: 'Black'},
+  {name: 'Thomas Surrey', age: 2, feet: '2\'', inches: '6 1/4"', colour: 'Black'},
+  {name: 'Lucinda Brown', age: 20, feet: '5\'', inches: '3"', colour: 'Yellow'},
+  {name: 'Mary Morris', age: 15, feet: '11\'', inches: '8 1/4"', colour: 'Yellow'},
+  {name: 'Clarasey Fletcher', age: 24, feet: '5\'', inches: '4 1/2"', colour: 'Black'},
+  {name: 'Dolly Armstead', age: 25, feet: '4\'', inches: '11 1/4"', colour: 'Black'},
+  {name: 'Mary Carter', age: 16, feet: '5\'', inches: '3 1/4"', colour: 'Black'},
+  {name: 'Elin Beal', age: 15, feet: '5\'', inches: '.."', colour: 'Black'},
+  {name: 'Secelia Brown', age: 22, feet: '5\'', inches: '3 1/2"', colour: 'Black'},
+  {name: 'Moses Murphy', age: 27, feet: '6\'', inches: '2 1/2"', colour: 'Black'},
+  {name: 'Thomas Johnson', age: 19, feet: '5\'', inches: '8"', colour: 'Black'},
+  {name: 'Peter Settles', age: 26, feet: '5\'', inches: '5"', colour: 'Black'},
+  {name: 'Amus Brown', age: 27, feet: '5\'', inches: '3"', colour: 'Black'},
+  {name: 'Aaron Carter', age: 24, feet: '5\'', inches: '7 1/4"', colour: 'Black'},
+  {name: 'Levy Williams', age: 20, feet: '5\'', inches: '5"', colour: 'Yellow'},
+  {name: 'Christopher Yates', age: 28, feet: '5\'', inches: '7 1/2"', colour: 'Black'},
+  {name: 'William Wilson', age: 17, feet: '5\'', inches: '1 1/2"', colour: 'Black'},
+  {name: 'Gabriel Singleton', age: 17, feet: '5\'', inches: '6 1/4"', colour: 'Black'},
+  {name: 'Martin ..', age: 16, feet: '5\'', inches: '.."', colour: 'Black'},
+  {name: 'George Miller', age: 18, feet: '5\'', inches: '2 1/2"', colour: 'Black'},
+  {name: 'Charles Brierly', age: 17, feet: '5\'', inches: '7"', colour: 'Yellow'},
+  {name: 'Lee ...', age: 12, feet: '4\'', inches: '4"', colour: 'Black'},
+  {name: 'Elijah Butler', age: 20, feet: '5\'', inches: '9"', colour: 'Black'},
+  {name: 'Mathew Hopewell', age: 16, feet: '5\'', inches: '.."', colour: 'Black'},
+  {name: 'Samuel Burges', age: 23, feet: '5\'', inches: '4 1/2"', colour: 'Black'},
+  {name: 'James Bauery', age: 22, feet: '5\'', inches: '8 3/4"', colour: 'Black'},
 ];
 
-
-// | 68 | Thomas Surrey | 2 | 2 | 6¼ | Black |
-
-// —page 3 —
-
-// Ship Manifest Listing the Enslaved, p. 2
-// | 69 | Lucinda Brown . . | 20 | 5 | 3 | Yellow |
-
-// | 70 | Mary Morris . . . | 15 | 11 | 8¼ | Yellow |
-
-// | 71 | Clarasey Fletcher . . | 24 | 5 | 4½ | Black|
-
-// | 72 | Dolly Armstead . . | 25 | 4 | 11¼ | Black |
-
-// | 73 | Mary Carter . . . | 16 | 5 | 3¼ | Black  |
-
-// | 74 | Elin Beale . . . | 15 | 5 | Black |
-
-// | 75 | Secelia Brown . . | 22 | 5 | 3½ | Black |
-
-// | 76 | Moses Murphy . . | 27 | 6 | 2½ | Black  |
-
-// | 77 | Thomas Johnson . . | 19 | 5 | 8 | Black |
-
-// | 78 | Peter Settles . . | 26 | 5 | 5 | Black |
-
-// | 79 | Amus Brown . . | 27 | 5 | 3 | Black |
-
-// | 80 | Aaaron Carter. . | 24 | 5 | 7¼ | Black |
-
-// | 81 | Levy Williams | 20 | 5 | 5 | Yellow |
-
-// | 82 | Christopher Yates | 28 | 5 | 7½ | Black|
-
-// | 83 | William Wilson | 17 | 5 | 1½ | Black |
-
-// | 84 | Gabriel Singleton | 17 | 5 | 6¼ | Black |
-
-// | 85 | Martin .  .  | 16 | 5 | .. | Black |
-
-// | 86 | George Miller | 18 | 5 | 2½  | Black |
-
-// | 87 | Charles Brierly | 17 | 5 | 7 | Yellow |
-
-// | 88 | Lee .  .  .  .  | 12 | 4 | 4 | Black|
-
-// | 89 | Elijah Butler | 20 | 5 | 9 | Black |
-
-// | 90 | Mathew Hopewell | 16 | 5 | .. | Black |
-
-// | 91 | Samuel Burges | 23 | 5 | 4½ | Black |
-
-// | 92 | James Bauery . . | 22 | 5 | 8¾ | Black |
