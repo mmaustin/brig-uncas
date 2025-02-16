42e2f75093721767c3c9b7d15d0068e1e99b6b2a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-02-16 11:30:37 -0500

    Added to transported humans array

--- uncas/constants/captiveArray.ts
+++ uncas/constants/captiveArray.ts
@@ -22,104 +22,52 @@ export const transportedHumans = [
   {name: 'Eliza Blackston', age: 12, feet: '4\'', inches: '5 1/2"', colour: 'Black'},
   {name: 'Ann Markston', age: 10, feet: '4\'', inches: '1"', colour: 'Black'},
   {name: 'Moriah Hawkins', age: 16, feet: '5\'', inches: '1/2"', colour: 'Black'},
+  {name: 'Sophy Smith', age: 19, feet: '4\'', inches: '..', colour: 'Black'},
+  {name: 'Benjamin Smith', age: 7, feet: '3\'', inches: '7"', colour: 'Black'},
+  {name: 'William Digs', age: 20, feet: '5\'', inches: '9 1/4"', colour: 'Black'},
+  {name: 'George Ford', age: 20, feet: '5\'', inches: '7 1/4"', colour: 'Black'},
+  {name: 'Dennez Griffin', age: 20, feet: '5\'', inches: '8"', colour: 'Black'},
+  {name: 'John Brown', age: 19, feet: '5\'', inches: '4"', colour: 'Black'},
+  {name: 'Vernon Louis', age: 19, feet: '5\'', inches: '4 1/2"', colour: 'Black'},
+  {name: 'Frederick Staley', age: 25, feet: '5\'', inches: '7 1/4"', colour: 'Black'},
+  {name: 'Dick Cothert', age: 20, feet: '5\'', inches: '7 1/2"', colour: 'Black'},
+  {name: 'Hanible Simms', age: 20, feet: '5\'', inches: '10 1/4"', colour: 'Black'},
+  {name: 'Aaron Harrison', age: 30, feet: '5\'', inches: '8 1/2"', colour: 'Black'},
+  {name: 'Andrew Scholfield', age: 21, feet: '5\'', inches: '5 1/2"', colour: 'Yellow'},
+  {name: 'Sampson ??', age: 21, feet: '5\'', inches: '4"', colour: 'Black'},
+  {name: 'Bib Gray', age: 22, feet: '5\'', inches: '5 1/2"', colour: 'Black'},
+  {name: 'Sawney Johnson', age: 23, feet: '5\'', inches: '5"', colour: 'Black'},
+  {name: 'Lloyd Steward', age: 18, feet: '5\'', inches: '4"', colour: 'Black'},
+  {name: 'Solloman Harriss', age: 20, feet: '5\'', inches: '2"', colour: 'Yellow'},
+  {name: 'William Bayer', age: 26, feet: '5\'', inches: '9 1/2"', colour: 'Black'},
+  {name: 'Reason Hanson', age: 21, feet: '5\'', inches: '7"', colour: 'Black'},
+  {name: 'Robert Green', age: 20, feet: '5\'', inches: '3 1/2"', colour: 'Black'},
+  {name: 'John Gordon', age: 19, feet: '5\'', inches: '8 3/4"', colour: 'Black'},
+  {name: 'Clarke Buttons', age: 26, feet: '5\'', inches: '3 1/2"', colour: 'Black'},
+  {name: 'Charles Simms', age: 26, feet: '5\'', inches: '7 3/4"', colour: 'Black'},
+  {name: 'Richard Howell', age: 33, feet: '5\'', inches: '8 3/4"', colour: 'Black'},
+  {name: 'Dawson Fairfax', age: 20, feet: '5\'', inches: '4 1/4"', colour: 'Black'},
+  {name: 'George Rioden', age: 25, feet: '5\'', inches: '8"', colour: 'Black'},
+  {name: 'January Penn', age: 21, feet: '5\'', inches: '3 1/4"', colour: 'Black'},
+  {name: 'Gusta Jackson', age: 21, feet: '5\'', inches: '7"', colour: 'Black'},
+  {name: 'John Johnson', age: 21, feet: '5\'', inches: '7"', colour: 'Black'},
+  {name: 'Henry Thornton', age: 14, feet: '4\'', inches: '8"', colour: 'Black'},
+  {name: 'Alfred Brisco', age: 15, feet: '4\'', inches: '10"', colour: 'Black'},
+  {name: 'Jane Tilman and Child', age: 20, feet: '5\'', inches: '3 1/4"', colour: 'Black'},
+  {name: 'Ann Moriah', age: 2, feet: '2\'', inches: '6"', colour: 'Black'},
+  {name: 'Sam Jackson', age: 14, feet: '4\'', inches: '4 3/4"', colour: 'Copper'},
+  {name: 'George Shorter', age: 13, feet: '4\'', inches: '7"', colour: 'Black'},
+  {name: 'Anton Burke', age: 13, feet: '4\'', inches: '7"', colour: 'Black'},
+  {name: 'Thomas Gaskins', age: 13, feet: '4\'', inches: '7"', colour: 'Black'},
+  {name: 'James Lewis', age: 12, feet: '4\'', inches: '4 1/2"', colour: 'Black'},
+  {name: 'Martin ...', age: 11, feet: '4\'', inches: '2 1/2"', colour: 'Black'},
+  {name: 'Mary Surrey', age: 40, feet: '5\'', inches: '1/2"', colour: 'Black'},
+  {name: 'Austine Surrey', age: 18, feet: '5\'', inches: '6 1/2"', colour: 'Black'},
+  {name: 'Charles Surrey', age: 15, feet: '4\'', inches: '11 1/2"', colour: 'Black'},
+  {name: 'Fredericks Surrey', age: 13, feet: '4\'', inches: '4 1/2"', colour: 'Black'},
+  {name: 'Margaret Ann Surrey', age: 9, feet: '3\'', inches: '6 1/4"', colour: 'Black'},
 ];
 
-
-// | 23 | Sophy Smith | 19 | 4 | .. | Black |
-
-// | 24 | Benjamin Smith | 7 | 3 | 7 | Black |
-
-// | 25 | William Digs | 20 | 5 | 9¼ | Black | |
-
-// | 26 | George Ford | 20 | 5 | 7¼ | Black |
-
-// | 27 | Dennez Griffin | 20 | 5 | 8 | Black |
-
-// | 28 | John Brown | 19 | 5 | 4 | Black |
-
-// | 29 | Vernon Louis | 19 | 5 | 4½ | Black |
-
-// | 30 | Frederick Staley | 25 | 5 | 7¼ | Black |
-
-// | 31 | Dick Cothert | 20 | 5 | 7½ | Black |
-
-// | 32 | Hanible Simms | 20 | 5 | 10¼ | Black |
-
-// | 33 | Aaron Harrison | 30 | 5 | 8½ | Black |
-
-// —Page 2—
-
-// [[right margin]] Shipped by Franklin & Armfield Alex. to Isaac Franklin New Orleans [[/right margin]]
-
-// | No . | Names of Slaves | Age | feet | Inches | Colour | Remarks |
-
-// | 34 | Andrew Scholfield | 21 | 5 | 5½ | yellow |
-
-// | 35 | Sampson [[??]] | 21 | 5 | 4 | Black |
-
-// | 36 | Bib Gray | 22 | 5 | 5½ | Black |
-
-// | 37 | Sawney Johnson | 23 | 5 | 5 | Black |
-
-// | 38 | Lloyd Stewart | 18 | 5 | 4 | Black |
-
-// | 39 | Solloman Harriss | 20 | 5 | 2 | yellow |
-
-// | 40 | Amos Hardon | 17 | 5 | 5 | Black |
-
-// | 41 | William Bayer | 26 | 5 | 9½ | Black |
-
-// | 42 | Reason Hanson | 21 | 5 | 7 | Black |
-
-// | 43 | Robert Green | 20 | 5 | 3 ½ | Black |
-
-// | 44 | John Gordon | 19 | 5 | 8¾ | Black |
-
-// | 45 | Clarke Buttons | 26 | 5 | 3½ | Black |
-
-// | 46 | Charles Simms | 26 | 5 | 7¾ | Black |
-
-// | 47 | Richard Howell | 33 | 5 | 8¾ | Black |
-
-// | 48 | Dawson Fairfax | 20 | 5 | 4¼ | Black |
-
-// | 49 | George Rioden [[best guess]] | 25 | 5 | 8 | Black |
-
-// | 50 | January Penn [[best guess]] | 21 | 5 | 3¼ | Black |
-
-// | 51 | Gusta Jackson | 21 | 5 | 7 | Black |
-
-// | 52 | John Johnson | 21 | 5 | 7 | Black |
-
-// | 53 | Henry Thornton | 14 | 4 | 8 | Black |
-
-// | 54 | Alfred Brisco | 15 | 4 | 10 | Black |
-
-// | 55 | Jane Tilman and child | 20 | 5 | 3¾ | Black |
-
-// | 56 | Ann Moriah | 2 | 2 | 6 | Black |
-
-// | 57 | Sam Jackson | 14 | 4 | 4¾ | Copper |
-
-// | 58 | George Shorter | 13 | 4 | 7 | Black |
-
-// | 59 | Anton Burke | 13 | 4 | 7 | Black |
-
-// | 60 | Thomas Gaskins | 13 | 4 | 7 | Black |
-
-// | 61 | James Lewis | 12 | 4 | 4½ | Black |
-
-// | 62 | Martin …. | 11 | 4 | 2½ | Black |
-
-// | 63 | Mary Surrey [[best guess]] | 40 | 5 ¼ | Black |
-
-// | 64 | Austine Surrey | 18 | 5 | 6½ | Black |
-
-// | 65 | Charles Surrey| 15 | 4 | 11½ | Black
-
-// | 66 | Fredericks Surrey | 13 | 4 | 4½ | Black |
-
-// | 67 | Margaret Ann Surrey [[best guess]] | 9 | 3 | 6¼ | Black |
 
 // | 68 | Thomas Surrey | 2 | 2 | 6¼ | Black |
 
