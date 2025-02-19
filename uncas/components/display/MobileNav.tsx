33605730827b3d3188f5b001fafcffa6ee78c234
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-02-18 20:51:29 -0500

    Resized according components in mobile nav

--- uncas/components/display/MobileNav.tsx
+++ uncas/components/display/MobileNav.tsx
@@ -9,16 +9,18 @@ const MobileNav = () => {
   const pathName = usePathname();
 
   return (
-    <nav>
+    <nav className="h-auto w-[275px]">
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="item-1">
-          <AccordionTrigger >All</AccordionTrigger>
+          <AccordionTrigger className="">Page Links</AccordionTrigger>
           <AccordionContent >
-            {links.map((link, i) => {
-              return <div key={link.name} className="">
-                <Link key={i} href={`${link.path}`} className={`${link.path === pathName && " text-red-500/60 text-xs no-underline font-light"} mt-2 capitalize`}>{link.name}</Link><br />
-              </div>
-            })}
+            <div className="w-full flex flex-wrap justify-between">
+              {links.map((link, i) => {
+                return <div key={link.name} className=" w-[135px]">
+                  <Link key={i} href={`${link.path}`} className={`${link.path === pathName && " text-red-500/60 text-xs no-underline font-light"} mt-2 text-xs`}>{link.name}</Link>
+                </div>
+              })}
+            </div>
           </AccordionContent>
         </AccordionItem>
       </Accordion>
