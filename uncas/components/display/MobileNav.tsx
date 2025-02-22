c6cce1e3e8dcddd847e1c2effe09869dfa2a7bfd
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-02-22 14:34:09 -0500

    Removed commented code from mobile nav component

--- uncas/components/display/MobileNav.tsx
+++ uncas/components/display/MobileNav.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { links } from "@/constants/captiveArray";
-//import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../ui/accordion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import {
@@ -11,7 +10,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-//import { cn } from "@/lib/utils";
 
 
 const MobileNav = () => {
@@ -20,36 +18,16 @@ const MobileNav = () => {
 
   return (
     <nav className="border">
-      {/* <Accordion type="single" collapsible className="w-full">
-        <AccordionItem value="item-1">
-          <AccordionTrigger className="">Page Links</AccordionTrigger>
-          <AccordionContent >
-            <div className="w-full flex flex-wrap justify-between">
-              {links.map((link, i) => {
-                return <div key={link.name} className=" w-[135px]">
-                  <Link key={i} href={`${link.path}`} className={`${link.path === pathName && "text-xs no-underline font-extrabold font-serif"} text-xs text font-light font-serif`}>{link.name}</Link>
-                </div>
-              })}
-            </div>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion> */}
       <DropdownMenu>
         <DropdownMenuTrigger >Open</DropdownMenuTrigger>
         <DropdownMenuContent>
           <DropdownMenuLabel>Routes</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          {/* <DropdownMenuItem><Link href="/manifest-front-L" >F</Link></DropdownMenuItem>
-          <DropdownMenuItem><Link href="/manifest-back-L" >B</Link></DropdownMenuItem> */}
           {links.map((link, i) => {
             return <DropdownMenuItem key={link.name} asChild>
               <Link key={i} href={`${link.path}`} className={`${link.path === pathName && "text-xs no-underline font-extrabold font-serif"} text-xs text font-light font-serif`}>{link.name}</Link>
             </DropdownMenuItem>
           })}
-          {/* <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem> */}
         </DropdownMenuContent>
       </DropdownMenu>
 
