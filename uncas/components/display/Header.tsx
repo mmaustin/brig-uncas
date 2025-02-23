77cf5a87ce76191404581c4e78a3d3a9a4e35f27
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-02-23 11:52:48 -0500

    Applied tailwind container property to both navbar components in the header component

--- uncas/components/display/Header.tsx
+++ uncas/components/display/Header.tsx
@@ -3,14 +3,14 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header className="flex justify-center items-center opacity-50 sticky top-0 backdrop-blur-md h-[120px] bg-white z-50">
-      <div className="container hidden md:flex items-center gap-8 rounded-full">
+    <header className="flex justify-center items-center opacity-50 sticky top-0 backdrop-blur-md h-[120px] bg-white z-50 border-2">
+      <div className="container hidden md:flex gap-8 rounded-full">
         <Navbar />
       </div>
-      <div className="md:hidden mt-6 md:mt-0 w-full flex justify-center items-center">
+      <div className="md:hidden w-full container">
         <MobileNav />
       </div>
     </header>
-  )
-}
+  );
+};
 export default Header;
