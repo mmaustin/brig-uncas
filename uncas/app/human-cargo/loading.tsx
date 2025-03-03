75a831b6f6b082c55d28ccdf2c6c3063ad8b58dd
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-03 17:02:22 -0500

    Hid full screen cover pic on md screens

--- uncas/app/human-cargo/loading.tsx
+++ uncas/app/human-cargo/loading.tsx
@@ -3,8 +3,12 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const loading = () => {
   return (
-    <div>
-      Please Wait . . . 
+    <div className="flex flex-col justify-center items-center space-y-3">
+      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
     </div>
   )
 }
--- uncas/app/human-cargo/page.tsx
+++ uncas/app/human-cargo/page.tsx
@@ -11,42 +11,42 @@ import {
 import { transportedHumans } from "@/constants/captiveArray";
 import Image from "next/image";
 import manifest from '@/public/manifest-8.png';
-//h-[725px] w-full
 
 const HumanCargo = () => {
   return (
-    <div className="">
-      
-      <div className="">
-        <Image
-          alt="Manifest Background"
-          src={manifest}
-          placeholder="blur"
-          quality={100}
-          sizes="100vw"
-          style={{
-            objectFit: 'cover',
-            position: 'absolute',
-            width: '100%',
-            height: '750px'
-          }}
-        />
-      </div>
-      <section className="h-screen w-screen my-8 flex justify-center items-center">
-        {/* <div className=" h-full w-full flex justify-center items-center border border-fuchsia-600"> */}
-        <Carousel className="mt-4 w-52">
-          <CarouselContent>
-            {transportedHumans.map((person, i) => {
-              return <CarouselItem key={i} className="capitalize font-serif italic text-lg font-bold opacity"><ShowCarousel captive={person} /></CarouselItem>
-            })}
-          </CarouselContent>
-          <CarouselPrevious className="opacity-30" />
-          <CarouselNext className="opacity-30" />
-        </Carousel>
+    <section>
+      <div className="md:hidden w-full container">      
+        <div className="">
+          <Image
+            alt="Manifest Background"
+            src={manifest}
+            placeholder="blur"
+            quality={100}
+            sizes="100vw"
+            style={{
+              objectFit: 'cover',
+              position: 'absolute',
+              width: '100%',
+              height: '750px'
+            }}
+          />
+        </div>
+        <section className="h-[450px] w-full my-8 flex justify-center items-center border border-red-500">
+          {/* <div className=" h-full w-full flex justify-center items-center border border-fuchsia-600"> */}
+          <Carousel className="mt-4 w-52">
+            <CarouselContent>
+              {transportedHumans.map((person, i) => {
+                return <CarouselItem key={i} className="capitalize font-serif italic text-lg font-bold opacity"><ShowCarousel captive={person} /></CarouselItem>
+              })}
+            </CarouselContent>
+            <CarouselPrevious className="opacity-30" />
+            <CarouselNext className="opacity-30" />
+          </Carousel>
+          {/* </div> */}
+        </section>
         {/* </div> */}
-      </section>
-      {/* </div> */}
-    </div>
+      </div>
+    </section>
   )
 }
 export default HumanCargo;
