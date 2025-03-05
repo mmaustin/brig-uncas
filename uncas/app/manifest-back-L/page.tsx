c836cba1080a5a75613d0b1ef19db887363fed93
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-05 11:54:07 -0500

    More grid troubleshooting

--- uncas/app/manifest-back-L/page.tsx
+++ uncas/app/manifest-back-L/page.tsx
@@ -1,34 +1,41 @@
-import Image from "next/image"
-import manifest from '@/assets/manifest-6.png';
-import manifestA from '@/assets/manifest-7.png';
+// import Image from "next/image"
+// import manifest from '@/assets/manifest-6.png';
+// import manifestA from '@/assets/manifest-7.png';
 
 
 const ManifestBackLeft = () => {
   return (
-    <div className="h-[500px] w-full flex justify-center items-center gap-8 border border-blue-700">
-      <div className="container justify-center items-center grid md:grid-cols-2 gap-8 border border-green-600">
-        {/* <div className="border w-10">words for words</div>
-        <div className="border w-10">words for words</div> */}
-        <Image
-          className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[575px]"
-          src={manifest}
-          alt="Manifest Back Left"
-          priority
-          height={265}
-          width={198}
-        />
-        <Image
-          className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[575px]"
-          src={manifestA}
-          alt="Manifest Back Left"
-          priority
-          height={265}
-          width={198}
-        />
-      </div>
-      {/* <div className="grid md:grid-cols-2 container justify-center items-center gap-8 border border-red-500"> */}
-      {/* </div> */}
+    <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-4">
+      <div className="border">01</div>
+      <div className="border">09</div>
+      <div className="border">09</div>
+      <div className="border">09</div>
+      <div className="border">09</div>
+      <div className="border">09</div>
+      <div className="border">09</div>
+      <div className="border">09</div>
+      <div className="border">09</div>
     </div>
+    // <div className="h-[500px] w-full flex justify-center items-center gap-8 border border-blue-700">
+    //   <div className="container justify-center items-center grid md:grid-cols-2 gap-8 border border-green-600">
+    //     <Image
+    //       className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[575px]"
+    //       src={manifest}
+    //       alt="Manifest Back Left"
+    //       priority
+    //       height={265}
+    //       width={198}
+    //     />
+    //     <Image
+    //       className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[575px]"
+    //       src={manifestA}
+    //       alt="Manifest Back Left"
+    //       priority
+    //       height={265}
+    //       width={198}
+    //     />
+    //   </div>
+    // </div>
   )
 }
 export default ManifestBackLeft;
