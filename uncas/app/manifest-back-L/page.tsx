f2c60cecc25d0538abd6c5d995aabe702ce89dca
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-05 10:51:47 -0500

    Fix grid class misalignment

--- uncas/app/manifest-back-L/page.tsx
+++ uncas/app/manifest-back-L/page.tsx
@@ -5,25 +5,29 @@ import manifestA from '@/assets/manifest-7.png';
 
 const ManifestBackLeft = () => {
   return (
-    <div className="h-full w-full flex flex-col justify-center items-center">
-      <div className="grid md:grid-cols-2 gap-8">
-      <Image
-        className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[575px]"
-        src={manifest}
-        alt="Manifest Back Left"
-        priority
-        height={265}
-        width={198}
-      />
-      <Image
-        className="h-[200px] w-[205px] mt-8"
-        src={manifestA}
-        alt="Manifest Back Left"
-        priority
-        height={265}
-        width={198}
-      />
+    <div className="h-[500px] w-full flex justify-center items-center gap-8 border border-blue-700">
+      <div className="container justify-center items-center grid md:grid-cols-2 gap-8 border border-green-600">
+        {/* <div className="border w-10">words for words</div>
+        <div className="border w-10">words for words</div> */}
+        <Image
+          className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[575px]"
+          src={manifest}
+          alt="Manifest Back Left"
+          priority
+          height={265}
+          width={198}
+        />
+        <Image
+          className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[575px]"
+          src={manifestA}
+          alt="Manifest Back Left"
+          priority
+          height={265}
+          width={198}
+        />
       </div>
+      {/* <div className="grid md:grid-cols-2 container justify-center items-center gap-8 border border-red-500"> */}
+      {/* </div> */}
     </div>
   )
 }
