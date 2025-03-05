79a1767259ad66407aa54a2bd732047aab5cc0c3
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-05 09:30:25 -0500

    Applied grid settings to first display page

--- uncas/app/manifest-back-L/page.tsx
+++ uncas/app/manifest-back-L/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image"
 import manifest from '@/assets/manifest-6.png';
-//import manifestA from '@/assets/manifest-7.png';
+import manifestA from '@/assets/manifest-7.png';
 
 
 const ManifestBackLeft = () => {
   return (
     <div className="h-full w-full flex flex-col justify-center items-center">
+      <div className="grid md:grid-cols-2 gap-8">
       <Image
         className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[575px]"
         src={manifest}
@@ -14,14 +15,15 @@ const ManifestBackLeft = () => {
         height={265}
         width={198}
       />
-      {/* <Image
-        className="h-[275px] w-[275px] mt-8"
+      <Image
+        className="h-[200px] w-[205px] mt-8"
         src={manifestA}
         alt="Manifest Back Left"
         priority
         height={265}
         width={198}
-      /> */}
+      />
+      </div>
     </div>
   )
 }
