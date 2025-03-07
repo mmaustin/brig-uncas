082f6a7d57bec1423b7dfea861a6c7894db03347
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-07 18:52:16 -0500

    Experimenting with grid sizing

--- uncas/app/manifest-back-L/page.tsx
+++ uncas/app/manifest-back-L/page.tsx
@@ -1,28 +1,44 @@
-import Image from "next/image"
-import manifest from '@/assets/manifest-6.png';
-import manifestA from '@/assets/manifest-7.png';
+//import Image from "next/image"
+// import manifest from '@/assets/manifest-6.png';
+// import manifestA from '@/assets/manifest-7.png';
 //import { Carousel, CarouselContent,CarouselItem, CarouselNext,CarouselPrevious, } from "@/components/ui/carousel";
 
 
 const ManifestBackLeft = () => {
   return (
-    <div className="grid sm:grid-cols-2 gap-4 mx-10 justify-center items-center ">
-      <Image
-        className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[575px]"
+    <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 mx-10 justify-center items-center border border-blue-600">
+      {/* <Image
+        className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[575px] border border-red-600"
         src={manifest}
         alt="Manifest Back Left"
         priority
         height={265}
         width={198}
-      />
-      <Image
+      /> */}
+      <div className="border flex flex-wrap justify-center items-center">
+        <p className="text-xs">Shipped by Franklin & Armfield Alex to Isaac Franklin New Orleans</p>
+        <p className="text-xs">District of Alexandria Port of Alexandria the 30th day of October 1833. I John Armfield owner of the persons named and particularly described in the within Manifest, and Joseph C Moore Master of the Brig Uncas do solemnly, sincerely, and truly swear, each of us, to the best of our knowledge and belief, that the Negroes within named, have not been imported into the United States, since the first day of January one thousand, eight hundred and eight; and that under the Laws of the United States are held to service or labor as slaves, and are not entitled to freedom under these Laws, at a certain time, and after a known prior of service —Sworn to this thirtieth day of October 1833. before C T. Chapman Collector So help us God. John Armfield Joseph C Moore </p>
+      </div>
+      <div className="border flex flex-wrap justify-center items-center">
+        <p className="text-xs">Shipped by Franklin & Armfield Alex to Isaac Franklin New Orleans</p>
+        <p className="text-xs">District of Alexandria Port of Alexandria the 30th day of October 1833. I John Armfield owner of the persons named and particularly described in the within Manifest, and Joseph C Moore Master of the Brig Uncas do solemnly, sincerely, and truly swear, each of us, to the best of our knowledge and belief, that the Negroes within named, have not been imported into the United States, since the first day of January one thousand, eight hundred and eight; and that under the Laws of the United States are held to service or labor as slaves, and are not entitled to freedom under these Laws, at a certain time, and after a known prior of service —Sworn to this thirtieth day of October 1833. before C T. Chapman Collector So help us God. John Armfield Joseph C Moore </p>
+      </div>
+      <div className="border flex flex-wrap justify-center items-center">
+        <p className="text-xs">Shipped by Franklin & Armfield Alex to Isaac Franklin New Orleans</p>
+        <p className="text-xs">District of Alexandria Port of Alexandria the 30th day of October 1833. I John Armfield owner of the persons named and particularly described in the within Manifest, and Joseph C Moore Master of the Brig Uncas do solemnly, sincerely, and truly swear, each of us, to the best of our knowledge and belief, that the Negroes within named, have not been imported into the United States, since the first day of January one thousand, eight hundred and eight; and that under the Laws of the United States are held to service or labor as slaves, and are not entitled to freedom under these Laws, at a certain time, and after a known prior of service —Sworn to this thirtieth day of October 1833. before C T. Chapman Collector So help us God. John Armfield Joseph C Moore </p>
+      </div>
+      {/* <div className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[575px] border flex flex-wrap justify-start items-center">
+        <p className="text-sm">Shipped by Franklin & Armfield Alex to Isaac Franklin New Orleans</p>
+        <p className="text-sm border">District of Alexandria Port of Alexandria the 30th day of October 1833. I John Armfield owner of the persons named and particularly described in the within Manifest, and Joseph C Moore Master of the Brig Uncas do solemnly, sincerely, and truly swear, each of us, to the best of our knowledge and belief, that the Negroes within named, have not been imported into the United States, since the first day of January one thousand, eight hundred and eight; and that under the Laws of the United States are held to service or labor as slaves, and are not entitled to freedom under these Laws, at a certain time, and after a known prior of service —Sworn to this thirtieth day of October 1833. before C T. Chapman Collector So help us God. John Armfield Joseph C Moore </p>
+      </div> */}
+      {/* <Image
         className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[575px]"
         src={manifestA}
         alt="Manifest Back Left"
         priority
         height={265}
         width={198}
-      />
+      /> */}
     </div>
   )
 }
