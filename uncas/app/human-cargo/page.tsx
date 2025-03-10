5ef7badec05c28860e755b156db946b1c89b1ce0
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-10 18:22:54 -0400

    Removed container image cover

--- uncas/app/human-cargo/page.tsx
+++ uncas/app/human-cargo/page.tsx
@@ -18,18 +18,13 @@ const HumanCargo = () => {
       <div className=" w-full container">      
         <div className="">
           <Image
-            alt="Manifest Background"
-            src={manifest}
-            placeholder="blur"
-            quality={100}
-            sizes="100vw"
-            style={{
-              objectFit: 'cover',
-              position: 'absolute',
-              width: '100%',
-              height: '750px'
-            }}
-          />
+        className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[575px] border border-red-600"
+        src={manifest}
+        alt="Manifest Back Left"
+        priority
+        height={265}
+        width={198}
+        />
         </div>
         <section className="h-[450px] w-screen my-8 flex justify-center items-center border border-red-500">
           <Carousel className="w-36 sm:w-52 sm:mt-4">
@@ -48,3 +43,17 @@ const HumanCargo = () => {
 }
 export default HumanCargo;
 
+//code for image to cover the screen
+{/* <Image
+  alt="Manifest Background"
+  src={manifest}
+  placeholder="blur"
+  quality={100}
+  sizes="100vw"
+  style={{
+    objectFit: 'cover',
+    position: 'absolute',
+    width: '100%',
+    height: '750px'
+  }}
+/> */}
