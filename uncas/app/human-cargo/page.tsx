8c1856460460e772cf64ce88271590165d2b515e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-10 18:58:19 -0400

    Adjust flex properties on pre-small screen view

--- uncas/app/human-cargo/page.tsx
+++ uncas/app/human-cargo/page.tsx
@@ -15,7 +15,7 @@ import manifest from '@/public/manifest-8.png';
 const HumanCargo = () => {
   return (
     <section className="w-full h-[550px]">
-      <div className=" w-inherit h-full mx-10 flex flex-col md:flex-wrap md:justify-around md:items-center border border-green-500">
+      <div className=" w-inherit h-full mx-10 flex flex-col justify-center  md:flex-wrap md:justify-around items-center border border-green-500">
         <div className="">
           <Image
             className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[425px] lg:w-[500px] border border-red-600"
