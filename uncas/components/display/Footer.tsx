8a69140827c91380947a90089b512dea2cf429c4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-13 20:21:23 -0400

    Pasted footer code from previous project

---
+++ uncas/components/display/Footer.tsx
@@ -0,0 +1,29 @@
+import { FaCopyright } from "react-icons/fa";
+import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
+
+const Footer = () => {
+
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="py-8 xl:py-12 text-white bg-red-500/60 lg:h-full flex justify-center items-center mt-10 mb-0">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="w-full flex flex-col md:flex-row md:justify-between items-center">
+          <div className="mb-4 md:mb-0 text-white flex flex-row justify-center items-center gap-6">
+            <p className="hover:text-red-700/60"><a href="mailto:mccrayaustin@ymail.com"><FaEnvelope /></a></p>
+            <p className="hover:text-red-700/60"><a href="tel:19736340148"><FaPhoneAlt /></a></p>
+          </div>
+          <div className="flex flex-row justify-center items-center gap-2 mb-4 md:mb-0">
+            <FaCopyright className="rounded-lg w-4 h-4 text-white/70" />
+            <p className="text-xs text-white">{year}</p>
+            <p className="text-xs text-white">McCray Austin</p>
+          </div>
+          <div className="">
+            Something Goes Here
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+export default Footer;
