0ef9b58e744436861b68a59e482fa39edd789c8c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-13 20:50:09 -0400

    Added an index property to the main captives array

--- uncas/constants/captiveArray.ts
+++ uncas/constants/captiveArray.ts
@@ -24,99 +24,132 @@ export const agesArray: AgesType[] = [
 ]
 
 export const transportedHumans = [
-  {name: 'Clawey Ford & 4 children', age: 30, feet: '5', inches: '5"', colour: 'Copper'},
-  {name: 'John D.', age: 7, feet: '4\'', inches: '..', colour: 'Copper'},
-  {name: 'Matty D.', age: 5, feet: '3\'', inches: '3 1/4"', colour: 'Copper'},
-  {name: 'Andrew D.', age: 4, feet: '2\'', inches: '10"', colour: 'Copper'},
-  {name: 'Clowey Ann', age: '9 months', feet: '2\'', inches: '10"', colour: 'Copper'},
-  {name: 'Eliza Smith', age: 19, feet: '5\'', inches: '10 1/4"', colour: 'Black'},
-  {name: 'Ann Mariah Hawkins', age: 20, feet: '5\'', inches: '3 1/2"', colour: 'Black'},
-  {name: 'Ann Adison', age: 19, feet: '5\'', inches: '1"', colour: 'Black'},
-  {name: 'Mary Clarke', age: 18, feet: '5\'', inches: '3"', colour: 'Black'},
-  {name: 'Elizabeth Ducket', age: 18, feet: '5\'', inches: '1"', colour: 'Black'},
-  {name: 'Louisa Fisher', age: 15, feet: '5\'', inches: '1"', colour: 'Black'},
-  {name: 'Anny Digs & 1 Child', age: 20, feet: '4\'', inches: '9"', colour: 'Black'},
-  {name: 'Henry', age: 2, feet: '2\'', inches: '4 1/2"', colour: 'Black'},
-  {name: 'Sharlotta Camphhour & 1 C', age: 25, feet: '5\'', inches: '..', colour: 'Copper'},
-  {name: 'Ann Moriah', age: '1 1/2', feet: '..', inches: '..', colour: 'Copper'},
-  {name: 'Melinda Davis & 1 Child', age: 21, feet: '4\'', inches: '11"', colour: 'Black'},
-  {name: 'Ann Morah', age: 2, feet: '2\'', inches: '7 1/2"', colour: 'Black'},
-  {name: 'Arlivia Brisco', age: 14, feet: '4\'', inches: '10 1/2"', colour: 'Black'},
-  {name: 'Henny Blackston', age: 17, feet: '5\'', inches: '3"', colour: 'Black'},
-  {name: 'Eliza Blackston', age: 12, feet: '4\'', inches: '5 1/2"', colour: 'Black'},
-  {name: 'Ann Markston', age: 10, feet: '4\'', inches: '1"', colour: 'Black'},
-  {name: 'Moriah Hawkins', age: 16, feet: '5\'', inches: '1/2"', colour: 'Black'},
-  {name: 'Sophy Smith', age: 19, feet: '4\'', inches: '..', colour: 'Black'},
-  {name: 'Benjamin Smith', age: 7, feet: '3\'', inches: '7"', colour: 'Black'},
-  {name: 'William Digs', age: 20, feet: '5\'', inches: '9 1/4"', colour: 'Black'},
-  {name: 'George Ford', age: 20, feet: '5\'', inches: '7 1/4"', colour: 'Black'},
-  {name: 'Dennez Griffin', age: 20, feet: '5\'', inches: '8"', colour: 'Black'},
-  {name: 'John Brown', age: 19, feet: '5\'', inches: '4"', colour: 'Black'},
-  {name: 'Vernon Louis', age: 19, feet: '5\'', inches: '4 1/2"', colour: 'Black'},
-  {name: 'Frederick Staley', age: 25, feet: '5\'', inches: '7 1/4"', colour: 'Black'},
-  {name: 'Dick Cothert', age: 20, feet: '5\'', inches: '7 1/2"', colour: 'Black'},
-  {name: 'Hanible Simms', age: 20, feet: '5\'', inches: '10 1/4"', colour: 'Black'},
-  {name: 'Aaron Harrison', age: 30, feet: '5\'', inches: '8 1/2"', colour: 'Black'},
-  {name: 'Andrew Scholfield', age: 21, feet: '5\'', inches: '5 1/2"', colour: 'Yellow'},
-  {name: 'Sampson ??', age: 21, feet: '5\'', inches: '4"', colour: 'Black'},
-  {name: 'Bib Gray', age: 22, feet: '5\'', inches: '5 1/2"', colour: 'Black'},
-  {name: 'Sawney Johnson', age: 23, feet: '5\'', inches: '5"', colour: 'Black'},
-  {name: 'Lloyd Steward', age: 18, feet: '5\'', inches: '4"', colour: 'Black'},
-  {name: 'Solloman Harriss', age: 20, feet: '5\'', inches: '2"', colour: 'Yellow'},
-  {name: 'Amos Hardon', age: 17, feet: '5\'', inches: '5"', colour: 'Black'},
-  {name: 'William Bayer', age: 26, feet: '5\'', inches: '9 1/2"', colour: 'Black'},
-  {name: 'Reason Hanson', age: 21, feet: '5\'', inches: '7"', colour: 'Black'},
-  {name: 'Robert Green', age: 20, feet: '5\'', inches: '3 1/2"', colour: 'Black'},
-  {name: 'John Gordon', age: 19, feet: '5\'', inches: '8 3/4"', colour: 'Black'},
-  {name: 'Clarke Buttons', age: 26, feet: '5\'', inches: '3 1/2"', colour: 'Black'},
-  {name: 'Charles Simms', age: 26, feet: '5\'', inches: '7 3/4"', colour: 'Black'},
-  {name: 'Richard Howell', age: 33, feet: '5\'', inches: '8 3/4"', colour: 'Black'},
-  {name: 'Dawson Fairfax', age: 20, feet: '5\'', inches: '4 1/4"', colour: 'Black'},
-  {name: 'George Rioden', age: 25, feet: '5\'', inches: '8"', colour: 'Black'},
-  {name: 'January Penn', age: 21, feet: '5\'', inches: '3 1/4"', colour: 'Black'},
-  {name: 'Gusta Jackson', age: 21, feet: '5\'', inches: '7"', colour: 'Black'},
-  {name: 'John Johnson', age: 21, feet: '5\'', inches: '7"', colour: 'Black'},
-  {name: 'Henry Thornton', age: 14, feet: '4\'', inches: '8"', colour: 'Black'},
-  {name: 'Alfred Brisco', age: 15, feet: '4\'', inches: '10"', colour: 'Black'},
-  {name: 'Jane Tilman and Child', age: 20, feet: '5\'', inches: '3 1/4"', colour: 'Black'},
-  {name: 'Ann Moriah', age: 2, feet: '2\'', inches: '6"', colour: 'Black'},
-  {name: 'Sam Jackson', age: 14, feet: '4\'', inches: '4 3/4"', colour: 'Copper'},
-  {name: 'George Shorter', age: 13, feet: '4\'', inches: '7"', colour: 'Black'},
-  {name: 'Anton Burke', age: 13, feet: '4\'', inches: '7"', colour: 'Black'},
-  {name: 'Thomas Gaskins', age: 13, feet: '4\'', inches: '7"', colour: 'Black'},
-  {name: 'James Lewis', age: 12, feet: '4\'', inches: '4 1/2"', colour: 'Black'},
-  {name: 'Martin ...', age: 11, feet: '4\'', inches: '2 1/2"', colour: 'Black'},
-  {name: 'Mary Surrey', age: 40, feet: '5\'', inches: '1/2"', colour: 'Black'},
-  {name: 'Austine Surrey', age: 18, feet: '5\'', inches: '6 1/2"', colour: 'Black'},
-  {name: 'Charles Surrey', age: 15, feet: '4\'', inches: '11 1/2"', colour: 'Black'},
-  {name: 'Fredericks Surrey', age: 13, feet: '4\'', inches: '4 1/2"', colour: 'Black'},
-  {name: 'Margaret Ann Surrey', age: 9, feet: '3\'', inches: '6 1/4"', colour: 'Black'},
-  {name: 'Thomas Surrey', age: 2, feet: '2\'', inches: '6 1/4"', colour: 'Black'},
-  {name: 'Lucinda Brown', age: 20, feet: '5\'', inches: '3"', colour: 'Yellow'},
-  {name: 'Mary Morris', age: 15, feet: '11\'', inches: '8 1/4"', colour: 'Yellow'},
-  {name: 'Clarasey Fletcher', age: 24, feet: '5\'', inches: '4 1/2"', colour: 'Black'},
-  {name: 'Dolly Armstead', age: 25, feet: '4\'', inches: '11 1/4"', colour: 'Black'},
-  {name: 'Mary Carter', age: 16, feet: '5\'', inches: '3 1/4"', colour: 'Black'},
-  {name: 'Elin Beal', age: 15, feet: '5\'', inches: '.."', colour: 'Black'},
-  {name: 'Secelia Brown', age: 22, feet: '5\'', inches: '3 1/2"', colour: 'Black'},
-  {name: 'Moses Murphy', age: 27, feet: '6\'', inches: '2 1/2"', colour: 'Black'},
-  {name: 'Thomas Johnson', age: 19, feet: '5\'', inches: '8"', colour: 'Black'},
-  {name: 'Peter Settles', age: 26, feet: '5\'', inches: '5"', colour: 'Black'},
-  {name: 'Amus Brown', age: 27, feet: '5\'', inches: '3"', colour: 'Black'},
-  {name: 'Aaron Carter', age: 24, feet: '5\'', inches: '7 1/4"', colour: 'Black'},
-  {name: 'Levy Williams', age: 20, feet: '5\'', inches: '5"', colour: 'Yellow'},
-  {name: 'Christopher Yates', age: 28, feet: '5\'', inches: '7 1/2"', colour: 'Black'},
-  {name: 'William Wilson', age: 17, feet: '5\'', inches: '1 1/2"', colour: 'Black'},
-  {name: 'Gabriel Singleton', age: 17, feet: '5\'', inches: '6 1/4"', colour: 'Black'},
-  {name: 'Martin ..', age: 16, feet: '5\'', inches: '.."', colour: 'Black'},
-  {name: 'George Miller', age: 18, feet: '5\'', inches: '2 1/2"', colour: 'Black'},
-  {name: 'Charles Brierly', age: 17, feet: '5\'', inches: '7"', colour: 'Yellow'},
-  {name: 'Lee ...', age: 12, feet: '4\'', inches: '4"', colour: 'Black'},
-  {name: 'Elijah Butler', age: 20, feet: '5\'', inches: '9"', colour: 'Black'},
-  {name: 'Mathew Hopewell', age: 16, feet: '5\'', inches: '.."', colour: 'Black'},
-  {name: 'Samuel Burges', age: 23, feet: '5\'', inches: '4 1/2"', colour: 'Black'},
-  {name: 'James Bauery', age: 22, feet: '5\'', inches: '8 3/4"', colour: 'Black'},
+  {index: 1, name: 'Clawey Ford & 4 children', age: 30, feet: '5', inches: '5"', colour: 'Copper'},
+  {index: 2, name: 'John D.', age: 7, feet: '4\'', inches: '..', colour: 'Copper'},
+  {index: 3, name: 'Matty D.', age: 5, feet: '3\'', inches: '3 1/4"', colour: 'Copper'},
+  {index: 4, name: 'Andrew D.', age: 4, feet: '2\'', inches: '10"', colour: 'Copper'},
+  {index: 5, name: 'Clowey Ann', age: '9 months', feet: '2\'', inches: '10"', colour: 'Copper'},
+  {index: 6, name: 'Eliza Smith', age: 19, feet: '5\'', inches: '10 1/4"', colour: 'Black'},
+  {index: 7, name: 'Ann Mariah Hawkins', age: 20, feet: '5\'', inches: '3 1/2"', colour: 'Black'},
+  {index: 8, name: 'Ann Adison', age: 19, feet: '5\'', inches: '1"', colour: 'Black'},
+  {index: 9, name: 'Mary Clarke', age: 18, feet: '5\'', inches: '3"', colour: 'Black'},
+  {index: 10, name: 'Elizabeth Ducket', age: 18, feet: '5\'', inches: '1"', colour: 'Black'},
+  {index: 11, name: 'Louisa Fisher', age: 15, feet: '5\'', inches: '1"', colour: 'Black'},
+  {index: 12, name: 'Anny Digs & 1 Child', age: 20, feet: '4\'', inches: '9"', colour: 'Black'},
+  {index: 13, name: 'Henry', age: 2, feet: '2\'', inches: '4 1/2"', colour: 'Black'},
+  {index: 14, name: 'Sharlotta Camphhour & 1 C', age: 25, feet: '5\'', inches: '..', colour: 'Copper'},
+  {index: 15, name: 'Ann Moriah', age: '1 1/2', feet: '..', inches: '..', colour: 'Copper'},
+  {index: 16, name: 'Melinda Davis & 1 Child', age: 21, feet: '4\'', inches: '11"', colour: 'Black'},
+  {index: 17, name: 'Ann Morah', age: 2, feet: '2\'', inches: '7 1/2"', colour: 'Black'},
+  {index: 18, name: 'Arlivia Brisco', age: 14, feet: '4\'', inches: '10 1/2"', colour: 'Black'},
+  {index: 19, name: 'Henny Blackston', age: 17, feet: '5\'', inches: '3"', colour: 'Black'},
+  {index: 20, name: 'Eliza Blackston', age: 12, feet: '4\'', inches: '5 1/2"', colour: 'Black'},
+  {index: 21, name: 'Ann Markston', age: 10, feet: '4\'', inches: '1"', colour: 'Black'},
+  {index: 22, name: 'Moriah Hawkins', age: 16, feet: '5\'', inches: '1/2"', colour: 'Black'},
+  {index: 23, name: 'Sophy Smith', age: 19, feet: '4\'', inches: '..', colour: 'Black'},
+  {index: 24, name: 'Benjamin Smith', age: 7, feet: '3\'', inches: '7"', colour: 'Black'},
+  {index: 25, name: 'William Digs', age: 20, feet: '5\'', inches: '9 1/4"', colour: 'Black'},
+  {index: 26, name: 'George Ford', age: 20, feet: '5\'', inches: '7 1/4"', colour: 'Black'},
+  {index: 27, name: 'Dennez Griffin', age: 20, feet: '5\'', inches: '8"', colour: 'Black'},
+  {index: 28, name: 'John Brown', age: 19, feet: '5\'', inches: '4"', colour: 'Black'},
+  {index: 29, name: 'Vernon Louis', age: 19, feet: '5\'', inches: '4 1/2"', colour: 'Black'},
+  {index: 30, name: 'Frederick Staley', age: 25, feet: '5\'', inches: '7 1/4"', colour: 'Black'},
+  {index: 31, name: 'Dick Cothert', age: 20, feet: '5\'', inches: '7 1/2"', colour: 'Black'},
+  {index: 32, name: 'Hanible Simms', age: 20, feet: '5\'', inches: '10 1/4"', colour: 'Black'},
+  {index: 33, name: 'Aaron Harrison', age: 30, feet: '5\'', inches: '8 1/2"', colour: 'Black'},
+  {index: 34, name: 'Andrew Scholfield', age: 21, feet: '5\'', inches: '5 1/2"', colour: 'Yellow'},
+  {index: 35, name: 'Sampson ??', age: 21, feet: '5\'', inches: '4"', colour: 'Black'},
+  {index: 36, name: 'Bib Gray', age: 22, feet: '5\'', inches: '5 1/2"', colour: 'Black'},
+  {index: 37, name: 'Sawney Johnson', age: 23, feet: '5\'', inches: '5"', colour: 'Black'},
+  {index: 38, name: 'Lloyd Steward', age: 18, feet: '5\'', inches: '4"', colour: 'Black'},
+  {index: 39, name: 'Solloman Harriss', age: 20, feet: '5\'', inches: '2"', colour: 'Yellow'},
+  {index: 40, name: 'Amos Hardon', age: 17, feet: '5\'', inches: '5"', colour: 'Black'},
+  {index: 41, name: 'William Bayer', age: 26, feet: '5\'', inches: '9 1/2"', colour: 'Black'},
+  {index: 42, name: 'Reason Hanson', age: 21, feet: '5\'', inches: '7"', colour: 'Black'},
+  {index: 43, name: 'Robert Green', age: 20, feet: '5\'', inches: '3 1/2"', colour: 'Black'},
+  {index: 44, name: 'John Gordon', age: 19, feet: '5\'', inches: '8 3/4"', colour: 'Black'},
+  {index: 45, name: 'Clarke Buttons', age: 26, feet: '5\'', inches: '3 1/2"', colour: 'Black'},
+  {index: 46, name: 'Charles Simms', age: 26, feet: '5\'', inches: '7 3/4"', colour: 'Black'},
+  {index: 47, name: 'Richard Howell', age: 33, feet: '5\'', inches: '8 3/4"', colour: 'Black'},
+  {index: 48, name: 'Dawson Fairfax', age: 20, feet: '5\'', inches: '4 1/4"', colour: 'Black'},
+  {index: 49, name: 'George Rioden', age: 25, feet: '5\'', inches: '8"', colour: 'Black'},
+  {index: 50, name: 'January Penn', age: 21, feet: '5\'', inches: '3 1/4"', colour: 'Black'},
+  {index: 51, name: 'Gusta Jackson', age: 21, feet: '5\'', inches: '7"', colour: 'Black'},
+  {index: 52, name: 'John Johnson', age: 21, feet: '5\'', inches: '7"', colour: 'Black'},
+  {index: 53, name: 'Henry Thornton', age: 14, feet: '4\'', inches: '8"', colour: 'Black'},
+  {index: 54, name: 'Alfred Brisco', age: 15, feet: '4\'', inches: '10"', colour: 'Black'},
+  {index: 55, name: 'Jane Tilman and Child', age: 20, feet: '5\'', inches: '3 1/4"', colour: 'Black'},
+  {index: 56, name: 'Ann Moriah', age: 2, feet: '2\'', inches: '6"', colour: 'Black'},
+  {index: 57, name: 'Sam Jackson', age: 14, feet: '4\'', inches: '4 3/4"', colour: 'Copper'},
+  {index: 58, name: 'George Shorter', age: 13, feet: '4\'', inches: '7"', colour: 'Black'},
+  {index: 59, name: 'Anton Burke', age: 13, feet: '4\'', inches: '7"', colour: 'Black'},
+  {index: 60, name: 'Thomas Gaskins', age: 13, feet: '4\'', inches: '7"', colour: 'Black'},
+  {index: 61, name: 'James Lewis', age: 12, feet: '4\'', inches: '4 1/2"', colour: 'Black'},
+  {index: 62, name: 'Martin ...', age: 11, feet: '4\'', inches: '2 1/2"', colour: 'Black'},
+  {index: 63, name: 'Mary Surrey', age: 40, feet: '5\'', inches: '1/2"', colour: 'Black'},
+  {index: 64, name: 'Austine Surrey', age: 18, feet: '5\'', inches: '6 1/2"', colour: 'Black'},
+  {index: 65, name: 'Charles Surrey', age: 15, feet: '4\'', inches: '11 1/2"', colour: 'Black'},
+  {index: 66, name: 'Fredericks Surrey', age: 13, feet: '4\'', inches: '4 1/2"', colour: 'Black'},
+  {index: 67, name: 'Margaret Ann Surrey', age: 9, feet: '3\'', inches: '6 1/4"', colour: 'Black'},
+  {index: 68, name: 'Thomas Surrey', age: 2, feet: '2\'', inches: '6 1/4"', colour: 'Black'},
+  {index: 69, name: 'Lucinda Brown', age: 20, feet: '5\'', inches: '3"', colour: 'Yellow'},
+  {index: 70, name: 'Mary Morris', age: 15, feet: '11\'', inches: '8 1/4"', colour: 'Yellow'},
+  {index: 71, name: 'Clarasey Fletcher', age: 24, feet: '5\'', inches: '4 1/2"', colour: 'Black'},
+  {index: 72, name: 'Dolly Armstead', age: 25, feet: '4\'', inches: '11 1/4"', colour: 'Black'},
+  {index: 73, name: 'Mary Carter', age: 16, feet: '5\'', inches: '3 1/4"', colour: 'Black'},
+  {index: 74, name: 'Elin Beal', age: 15, feet: '5\'', inches: '.."', colour: 'Black'},
+  {index: 75, name: 'Secelia Brown', age: 22, feet: '5\'', inches: '3 1/2"', colour: 'Black'},
+  {index: 76, name: 'Moses Murphy', age: 27, feet: '6\'', inches: '2 1/2"', colour: 'Black'},
+  {index: 77, name: 'Thomas Johnson', age: 19, feet: '5\'', inches: '8"', colour: 'Black'},
+  {index: 78, name: 'Peter Settles', age: 26, feet: '5\'', inches: '5"', colour: 'Black'},
+  {index: 79, name: 'Amus Brown', age: 27, feet: '5\'', inches: '3"', colour: 'Black'},
+  {index: 80, name: 'Aaron Carter', age: 24, feet: '5\'', inches: '7 1/4"', colour: 'Black'},
+  {index: 81, name: 'Levy Williams', age: 20, feet: '5\'', inches: '5"', colour: 'Yellow'},
+  {index: 82, name: 'Christopher Yates', age: 28, feet: '5\'', inches: '7 1/2"', colour: 'Black'},
+  {index: 83, name: 'William Wilson', age: 17, feet: '5\'', inches: '1 1/2"', colour: 'Black'},
+  {index: 84, name: 'Gabriel Singleton', age: 17, feet: '5\'', inches: '6 1/4"', colour: 'Black'},
+  {index: 85, name: 'Martin ..', age: 16, feet: '5\'', inches: '.."', colour: 'Black'},
+  {index: 86, name: 'George Miller', age: 18, feet: '5\'', inches: '2 1/2"', colour: 'Black'},
+  {index: 87, name: 'Charles Brierly', age: 17, feet: '5\'', inches: '7"', colour: 'Yellow'},
+  {index: 88, name: 'Lee ...', age: 12, feet: '4\'', inches: '4"', colour: 'Black'},
+  {index: 89, name: 'Elijah Butler', age: 20, feet: '5\'', inches: '9"', colour: 'Black'},
+  {index: 90, name: 'Mathew Hopewell', age: 16, feet: '5\'', inches: '.."', colour: 'Black'},
+  {index: 91, name: 'Samuel Burges', age: 23, feet: '5\'', inches: '4 1/2"', colour: 'Black'},
+  {index: 92, name: 'James Bauery', age: 22, feet: '5\'', inches: '8 3/4"', colour: 'Black'},
 ];
+
+
+
+export const links = [
+  {
+    name: "Names-Front",
+    path: '/names-front'
+  },
+  {
+    name: "Names-Back",
+    path: '/names-back'
+  },
+  {
+    name: "Text-Front",
+    path: '/text-front'
+  },
+  {
+    name: "Text-Back",
+    path: '/text-back'
+  },
+  {
+    name: "Ages",
+    path: '/ages'
+  }
+];
+
+export const backpageText = [
+  "Shipped by Franklin & Armfield Alex to Isaac Franklin New Orleans",
+  "District of Alexandria Port of Alexandria the 30th day of October 1833. I John Armfield owner of the persons named and particularly described in the within Manifest, and Joseph C Moore Master of the Brig Uncas do solemnly, sincerely, and truly swear, each of us, to the best of our knowledge and belief, that the Negroes within named, have not been imported into the United States, since the first day of January one thousand, eight hundred and eight; and that under the Laws of the United States are held to service or labor as slaves, and are not entitled to freedom under these Laws, at a certain time, and after a known prior of service —Sworn to this thirtieth day of October 1833. before C T. Chapman Collector So help us God. John Armfield Joseph C Moore "
+];
+
+
+
 
 // type CA = {
 //   one: number,
@@ -153,32 +186,3 @@ export const transportedHumans = [
 //   return ca;
 // };
 // console.log(countAges())
-
-
-export const links = [
-  {
-    name: "Names-Front",
-    path: '/names-front'
-  },
-  {
-    name: "Names-Back",
-    path: '/names-back'
-  },
-  {
-    name: "Text-Front",
-    path: '/text-front'
-  },
-  {
-    name: "Text-Back",
-    path: '/text-back'
-  },
-  {
-    name: "Ages",
-    path: '/ages'
-  }
-];
-
-export const backpageText = [
-  "Shipped by Franklin & Armfield Alex to Isaac Franklin New Orleans",
-  "District of Alexandria Port of Alexandria the 30th day of October 1833. I John Armfield owner of the persons named and particularly described in the within Manifest, and Joseph C Moore Master of the Brig Uncas do solemnly, sincerely, and truly swear, each of us, to the best of our knowledge and belief, that the Negroes within named, have not been imported into the United States, since the first day of January one thousand, eight hundred and eight; and that under the Laws of the United States are held to service or labor as slaves, and are not entitled to freedom under these Laws, at a certain time, and after a known prior of service —Sworn to this thirtieth day of October 1833. before C T. Chapman Collector So help us God. John Armfield Joseph C Moore "
-];
