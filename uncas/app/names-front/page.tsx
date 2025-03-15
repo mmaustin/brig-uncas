4f3b626cfff71ed0b51c3256a704fb3adec4dc8c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-15 13:44:18 -0400

    Moved names-front code to proper route

--- uncas/app/names-front/page.tsx
+++ uncas/app/names-front/page.tsx
@@ -8,17 +8,18 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { transportedHumans } from "@/constants/captiveArray";
+import { transportedHumansFront } from "@/constants/captiveArray";
 import Image from "next/image";
 import manifest from '@/public/manifest-8.png';
 
 const NamesFront = () => {
   return (
     <section className="w-full h-[550px]">
-      <div className=" w-inherit h-full mx-10 flex flex-col justify-center  md:flex-wrap md:justify-around items-center border border-green-500">
-        <div className="">
+      <div className=" w-inherit h-full mx-10 flex flex-col justify-center  md:flex-wrap md:justify-around items-center">
+        <div className="flex flex-col justify-center items-center">
+          <div className="text-center font-serif italic text-sm rounded-lg w-[275px] sm:w-[400px] md:w-[425px] lg:w-[500px] bg-my-beige border mx-2">No. / Name of Negroes / Age / Feet / Inches / Colour / Remarks</div>
           <Image
-            className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[425px] lg:w-[500px] border border-red-600"
+            className="rounded-lg h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[425px] lg:w-[500px]"
             src={manifest}
             alt="Manifest Back Left"
             priority
@@ -26,11 +27,11 @@ const NamesFront = () => {
             width={198}
           />
         </div>
-        <section className="h-[250px] w-[200px] my-8 flex justify-center items-center">
-          <Carousel className="w-36 sm:w-44 sm:mt-4">
+        <section className="h-[250px] w-[200px] my-8 flex justify-center items-center bg-my-beige rounded-lg">
+          <Carousel className="w-36 sm:w-44 mt-4">
             <CarouselContent>
-              {transportedHumans.map((person, i) => {
-                return <CarouselItem key={i} className="capitalize font-serif italic text-lg font-bold opacity"><ShowCarousel captive={person} /></CarouselItem>
+              {transportedHumansFront.map((person, i) => {
+                return <CarouselItem key={i} className="capitalize font-serif italic text-lg font-bold opacity "><ShowCarousel captive={person} /></CarouselItem>
               })}
             </CarouselContent>
             <CarouselPrevious className="opacity-30" />
