5f6107510c972230f189a47c795fed7c86e789d5
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-16 12:44:57 -0400

    Transported text-front route code to each of the other three display routes

--- uncas/app/names-back/page.tsx
+++ uncas/app/names-back/page.tsx
@@ -14,12 +14,11 @@ import manifest from '@/public/manifest-6.png';
 
 const NamesBack = () => {
   return (
-    <section className="w-full h-[550px]">
+    <section className="w-full h-[650px] md:h-[550px]">
       <div className=" w-inherit h-full mx-10 flex flex-col justify-center  md:flex-wrap md:justify-around items-center">
         <div className="flex flex-col justify-center items-center">
-          {/* <div className="text-center font-serif italic text-sm rounded-lg w-[275px] sm:w-[400px] md:w-[425px] lg:w-[500px] bg-my-beige border mx-2">No. / Name of Negroes / Age / Feet / Inches / Colour / Remarks</div> */}
           <Image
-            className="rounded-lg h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[425px] lg:w-[500px]"
+            className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[425px] lg:w-[500px]"
             src={manifest}
             alt="Manifest Back Left"
             priority
@@ -27,11 +26,11 @@ const NamesBack = () => {
             width={198}
           />
         </div>
-        <section className="h-[250px] w-[200px] my-8 flex justify-center items-center bg-my-beige rounded-lg">
+        <section className="h-[250px] w-[200px] my-8 flex justify-center items-center bg-green-800/80 rounded-lg">
           <Carousel className="w-36 sm:w-44 mt-4">
             <CarouselContent>
               {transportedHumansBack.map((person, i) => {
-                return <CarouselItem key={i} className="capitalize font-serif italic text-lg font-bold opacity "><ShowCarousel captive={person} /></CarouselItem>
+                return <CarouselItem key={i} className="text-white/80 capitalize font-serif italic text-lg font-bold opacity "><ShowCarousel captive={person} /></CarouselItem>
               })}
             </CarouselContent>
             <CarouselPrevious className="opacity-30" />
--- uncas/app/text-back/page.tsx
+++ uncas/app/text-back/page.tsx
@@ -8,17 +8,18 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { transportedHumans } from "@/constants/captiveArray";
+import { transportedHumansBack } from "@/constants/captiveArray";
 import Image from "next/image";
-import manifest from '@/public/manifest-8.png';
+import manifest from '@/public/manifest-6.png';
 
 const TextBack = () => {
   return (
-    <section className="w-full h-[550px]">
-      <div className=" w-inherit h-full mx-10 flex flex-col justify-center  md:flex-wrap md:justify-around items-center border border-green-500">
-        <div className="">
+    <section className="w-full h-[650px] md:h-[550px]">
+      <div className=" w-inherit h-full mx-10 flex flex-col justify-center  md:flex-wrap md:justify-around items-center">
+        <div className="flex flex-col justify-center items-center">
+          <div className="text-center font-serif italic text-sm w-[275px] sm:w-[400px] md:w-[425px] lg:w-[500px] text-white/80 bg-green-800/80 mx-2"><span className="text-yellow-300">*</span> No. / Name of Negroes / Age / Feet / Inches / Colour / Remarks</div>
           <Image
-            className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[425px] lg:w-[500px] border border-red-600"
+            className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[425px] lg:w-[500px]"
             src={manifest}
             alt="Manifest Back Left"
             priority
@@ -26,11 +27,11 @@ const TextBack = () => {
             width={198}
           />
         </div>
-        <section className="h-[250px] w-[200px] my-8 flex justify-center items-center">
-          <Carousel className="w-36 sm:w-44 sm:mt-4">
+        <section className="h-[250px] w-[200px] my-8 flex justify-center items-center bg-green-800/80 rounded-lg">
+          <Carousel className="w-36 sm:w-44 mt-4">
             <CarouselContent>
-              {transportedHumans.map((person, i) => {
-                return <CarouselItem key={i} className="capitalize font-serif italic text-lg font-bold opacity"><ShowCarousel captive={person} /></CarouselItem>
+              {transportedHumansBack.map((person, i) => {
+                return <CarouselItem key={i} className="text-white/80 capitalize font-serif italic text-lg font-bold opacity "><ShowCarousel captive={person} /></CarouselItem>
               })}
             </CarouselContent>
             <CarouselPrevious className="opacity-30" />
