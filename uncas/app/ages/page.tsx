facc47b45f27efc2bef128fc2401fa2e7090c246
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-16 12:58:07 -0400

    Enlarged ages route height to press footer to the bottom

--- uncas/app/ages/page.tsx
+++ uncas/app/ages/page.tsx
@@ -2,7 +2,7 @@ import AgesChart from "@/components/display/AgesChart";
 
 const AgesContainer = () => {
   return (
-    <div className="h-full w-full flex justify-center items-center">
+    <div className="h-[500px] w-full flex justify-center items-center">
       <AgesChart />
     </div>
   )
