0e8934706145d31a3d5042e4ac809b72b480ac5f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-22 15:27:40 -0400

    Used a different recharts pie chart to display distribution by age

--- uncas/components/display/AgesChart.tsx
+++ uncas/components/display/AgesChart.tsx
@@ -6,22 +6,38 @@ import { PieChart, Pie, Tooltip, ResponsiveContainer } from 'recharts';
 
 const AgesChart = () => {
   return (
-    <ResponsiveContainer width='100%' height={450} className='mx-4'>
-      <PieChart width={400} height={400} >
+    <ResponsiveContainer width="100%" height="100%">
+      <PieChart width={400} height={400}>
         <Pie
           dataKey="value"
-          isAnimationActive={false}
+          startAngle={180}
+          endAngle={0}
           data={agesArray}
           cx="50%"
           cy="50%"
-          innerRadius={60}
-          outerRadius={80}
-          fill="#231adb"
+          outerRadius={100}
+          fill="#473fde"
           label
         />
         <Tooltip wrapperStyle={{borderRadius: '25px'}} itemStyle={{color: 'green'}} offset={50}/>
-      </PieChart >
+      </PieChart>
     </ResponsiveContainer>
+    // <ResponsiveContainer width='100%' height={450} className='mx-4'>
+    //   <PieChart width={400} height={400} >
+    //     <Pie
+    //       dataKey="value"
+    //       isAnimationActive={false}
+    //       data={agesArray}
+    //       cx="50%"
+    //       cy="50%"
+    //       innerRadius={60}
+    //       outerRadius={80}
+    //       fill="#231adb"
+    //       label
+    //     />
+    //     <Tooltip wrapperStyle={{borderRadius: '25px'}} itemStyle={{color: 'green'}} offset={50}/>
+    //   </PieChart >
+    // </ResponsiveContainer>
   )
 }
 export default AgesChart
