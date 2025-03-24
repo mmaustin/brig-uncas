7e043d1bfe7666a873fa90e78816d7e1cd74379e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-24 13:28:04 -0400

    Misc changes

--- uncas/components/display/AgesChart.tsx
+++ uncas/components/display/AgesChart.tsx
@@ -6,6 +6,7 @@ import { PieChart, Pie, Tooltip, ResponsiveContainer } from 'recharts';
 
 const AgesChart = () => {
   return (
+    <>
     <ResponsiveContainer width="100%" height="100%">
       <PieChart width={400} height={400}>
         <Pie
@@ -16,28 +17,29 @@ const AgesChart = () => {
           cx="50%"
           cy="50%"
           outerRadius={100}
-          fill="#473fde"
+          fill="#231adb"
           label
         />
         <Tooltip wrapperStyle={{borderRadius: '25px'}} itemStyle={{color: 'green'}} offset={50}/>
       </PieChart>
     </ResponsiveContainer>
-    // <ResponsiveContainer width='100%' height={450} className='mx-4'>
-    //   <PieChart width={400} height={400} >
-    //     <Pie
-    //       dataKey="value"
-    //       isAnimationActive={false}
-    //       data={agesArray}
-    //       cx="50%"
-    //       cy="50%"
-    //       innerRadius={60}
-    //       outerRadius={80}
-    //       fill="#231adb"
-    //       label
-    //     />
-    //     <Tooltip wrapperStyle={{borderRadius: '25px'}} itemStyle={{color: 'green'}} offset={50}/>
-    //   </PieChart >
-    // </ResponsiveContainer>
+    {/* <ResponsiveContainer width='100%' height={450} className='mx-4'>
+      <PieChart width={400} height={400} >
+        <Pie
+          dataKey="value"
+          isAnimationActive={false}
+          data={agesArray}
+          cx="50%"
+          cy="50%"
+          innerRadius={60}
+          outerRadius={80}
+          fill="#231adb"
+          label
+        />
+        <Tooltip wrapperStyle={{borderRadius: '25px'}} itemStyle={{color: 'green'}} offset={50}/>
+      </PieChart >
+    </ResponsiveContainer> */}
+    </>
   )
 }
 export default AgesChart
