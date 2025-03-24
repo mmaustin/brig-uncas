f302689a4ef06ab96d50b92730685b75a8eb6c98
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-24 12:10:57 -0400

    Resized image and scroll field size on text front route

--- uncas/app/text-front/page.tsx
+++ uncas/app/text-front/page.tsx
@@ -11,9 +11,9 @@ const TextFront = () => {
     <section className="w-full h-[650px] md:h-[550px]">
       <div className=" w-inherit h-full mx-10 flex flex-col justify-center  md:flex-wrap md:justify-around items-center">
         <div className="flex flex-col justify-center items-center">
-          <div className="text-center font-serif italic text-sm w-[275px] sm:w-[400px] md:w-[425px] lg:w-[500px] text-white/80 bg-green-800/80 mx-2"><span className="text-yellow-300">*</span> No. / Name of Negroes / Age / Feet / Inches / Colour / Remarks</div>
+          <div className="text-center font-serif italic text-sm w-[275px] sm:w-[400px] md:w-[400px] lg:w-[500px] text-white/80 bg-green-800/80 mx-2"><span className="text-yellow-300">*</span> No. / Name of Negroes / Age / Feet / Inches / Colour / Remarks</div>
           <Image
-            className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[375px] md:w-[425px] lg:w-[500px]"
+            className="h-[200px] w-[275px] sm:h-[250px] sm:w-[400px] md:h-[355px] md:w-[400px] lg:h-[375px] lg:w-[500px]"
             src={manifest}
             alt="Manifest Back Left"
             priority
@@ -21,7 +21,7 @@ const TextFront = () => {
             width={198}
           />
         </div>
-        <ScrollArea className="h-[250px] w-[200px] sm:w-[250px] my-8 flex justify-center items-center bg-green-800/80 rounded-lg">
+        <ScrollArea className="h-[250px] w-[200px] sm:w-[250px] md:w-[250px] lg:w-[300px] my-8 flex justify-center items-center bg-green-800/80 rounded-lg">
           {frontpageText.map((text, i) => (
             <section key={i} className="m-2">
               <div key={i} className="text-sm text-white/80 font-serif font-semibold italic">
