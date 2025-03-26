e3f86c89deee0a16d4b493533dfd7a34fdec956e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-25 20:45:50 -0400

    Test code for popover on home page

--- uncas/app/page.tsx
+++ uncas/app/page.tsx
@@ -1,10 +1,31 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import houseThree from '@/public/house-3.png';
+import Image from "next/image";
 
 
-export default function Home() { 
+
+export default function Home() {
 
   return (
-   <div className="w-full h-screen flex justify-center items-center text-lg font-serif">
-    Home Page
-   </div>
+    <div className="w-full h-screen flex justify-center items-center text-lg font-serif">
+      <Popover>
+        <PopoverTrigger>
+          <Image
+            className="h-[200px] w-[275px]"
+            src={houseThree}
+            alt="Manifest Back Left"
+            priority
+            height={265}
+            width={198}
+          />
+        </PopoverTrigger>
+        <PopoverContent>Place content for the popover here.</PopoverContent>
+      </Popover>
+
+    </div>
   );
 };
