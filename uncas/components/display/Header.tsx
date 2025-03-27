6ea78fd3325753fc51d0f7dd29b189e7840954fd
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-27 18:32:00 -0400

    Decreased header height, x and y centered elements

--- uncas/components/display/Header.tsx
+++ uncas/components/display/Header.tsx
@@ -3,8 +3,8 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header className="flex justify-center items-center opacity-50 sticky top-0 backdrop-blur-md h-[120px] bg-green-800 z-50">
-      <div className="container hidden md:flex gap-8 rounded-full">
+    <header className="flex justify-center items-center opacity-50 sticky top-0 backdrop-blur-md h-[100px] bg-green-800 z-50">
+      <div className="hidden md:flex gap-8 w-full">
         <Navbar />
       </div>
       <div className="md:hidden w-full container mx-4">
--- uncas/components/display/Navbar.tsx
+++ uncas/components/display/Navbar.tsx
@@ -9,25 +9,19 @@ const Navbar = () => {
   const pathName = usePathname();
 
   return (
-    <div className="flex flex-col justify-center items-center w-full rounded-full backdrop-blur-xl bg-green-800">
-      <div className="flex flex-row justify-between items-center mb-8 w-full">
+    <div className="items-center mx-8 w-full backdrop-blur-xl">
+      <div className="flex justify-between items-center mb-8 w-full h-full">
         <h1 className="">
           <Link href={'/'} className="text-2xl font-extrabold font-serif uppercase text-white" >Brig Uncas</Link>
-        </h1>
-        <div className="text-white">
-          Links To Something
-        </div>
+        </h1>    
+        <nav className="flex gap-6">
+          {links.map((link, i) => {
+            return (
+              <Link key={i} href={link.path} className={`${link.path === pathName && " text-lg text-yellow-300 no-underline font-extrabold font-serif uppercase"} text-white text-sm font-light font-serif`}>{link.name}</Link>
+            )
+          })}
+        </nav>
       </div>
-      <nav className="flex gap-8">
-        {links.map((link, i) => {
-          return (
-            <Link key={i} href={link.path} className={`${link.path === pathName && " text-lg text-yellow-300 no-underline font-extrabold font-serif uppercase"} text-white text-sm font-light font-serif`}>{link.name}</Link>
-          )
-        })}
-      </nav>
-        {/* {pathName === '/human-cargo' && 
-          <div className="h-8 w-full border">Number Name Age Feet Inches Colour</div>
-        } */}
     </div>
   )
 }
