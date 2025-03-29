785522785f55b0ffa1441079c0fa94ea1c906897
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-29 15:19:58 -0400

    Corrected small spelling error

--- uncas/components/display/HomePageText.tsx
+++ uncas/components/display/HomePageText.tsx
@@ -5,7 +5,7 @@ const HomePageText = () => {
   return (
     <div className="mb-4 h-auto w-full flex flex-col justify-center items-center font-serif font-semibold italic">
       <p className="w-[90%] text-sm sm:text-base md:text-lg">
-        The Brig Uncas was one of three ships that Alexandria, VA based domestic slave trading firm, Franklin & Armfield, used to transport enslaved humans from the Upper South, mainly Virginia and Maryland, to the Port of New Orleans.  There, the enslaved would be bought, resold, mortgaged, etc., and disburded throughout the Lower South according to the owner&apos;s dictate. <span className="text-xs not-italic font-normal">No picture of the Brig Uncas, of which I&apos;m aware, survives.</span>
+        The Brig Uncas was one of three ships that Alexandria, VA based domestic slave trading firm, Franklin & Armfield, used to transport enslaved humans from the Upper South, mainly Virginia and Maryland, to the Port of New Orleans.  There, the enslaved would be bought, resold, mortgaged, etc., and dispersed throughout the Lower South according to the owner&apos;s dictate. <span className="text-xs not-italic font-normal">No picture of the Brig Uncas, of which I&apos;m aware, survives.</span>
       </p>
       <p className="mt-4 text-xs sm:hidden font-normal text-green-800 capitalize">tap image for caption</p>
     </div>
