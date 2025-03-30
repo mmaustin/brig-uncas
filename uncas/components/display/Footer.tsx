4241c423e17fae03181a93cd100105b4a4cdb285
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-30 10:47:02 -0400

    Added homepage link to footer

--- uncas/components/display/Footer.tsx
+++ uncas/components/display/Footer.tsx
@@ -1,5 +1,5 @@
+import Link from "next/link";
 import { FaCopyright } from "react-icons/fa";
-import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
 
@@ -9,9 +9,8 @@ const Footer = () => {
     <footer className="py-8 xl:py-12 text-white bg-green-800/80 lg:h-full flex justify-center items-center mt-10 mb-0">
       <div className="w-[90%] flex justify-between items-center">
         <div className="w-full flex flex-col md:flex-row md:justify-between items-center">
-          <div className="w-[200px] mb-4 md:mb-0 text-white flex flex-row justify-center items-center gap-6">
-            <p className="hover:text-red-700/60"><a href="mailto:mccrayaustin@ymail.com"><FaEnvelope /></a></p>
-            <p className="hover:text-red-700/60"><a href="tel:19736340148"><FaPhoneAlt /></a></p>
+          <div className="">
+            <Link href={'/'} className="text-xs text-yellow-300 font-extrabold font-serif uppercase text-white" >Brig Uncas</Link>
           </div>
           <div className="w-[200px] flex flex-row justify-center items-center gap-2 mb-4 md:mb-0">
             <FaCopyright className="rounded-lg w-4 h-4 text-white/70" />
