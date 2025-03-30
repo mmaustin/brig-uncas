3cbcbb550479cf65d7835fd474cac4b4e171addd
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-30 11:59:57 -0400

    Adjusted gaps and flex of new footer elements

--- uncas/components/display/Footer.tsx
+++ uncas/components/display/Footer.tsx
@@ -10,11 +10,12 @@ const Footer = () => {
   return (
     <footer className="py-8 xl:py-12 text-white bg-green-800/80 lg:h-full flex justify-center items-center mt-10 mb-0">
       <div className="w-[90%] flex justify-between items-center">
-        <div className="w-full flex flex-col md:flex-row md:justify-between items-center">
+        <div className="w-full flex flex-col gap-2 sm:gap-0 sm:flex-row sm:justify-between items-center">
+        {/* <div className="w-full flex flex-row justify-between items-center"> */}
           <div className="">
             <Link href={'/'} className="text-xs text-yellow-300 font-extrabold font-serif uppercase text-white" >Brig Uncas</Link>
           </div>
-          <div className="w-[200px] flex flex-row justify-center items-center gap-2 mb-4 md:mb-0">
+          <div className="w-[200px] flex flex-row justify-center items-center gap-2 ">
             <FaCopyright className="rounded-lg w-4 h-4 text-white/70" />
             <p className="text-xs text-white">{year}</p>
             <p className="text-xs text-white">McCray Austin</p>
