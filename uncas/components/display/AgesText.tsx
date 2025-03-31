e3436f06bd92c414f012edbac909c62b4b887c07
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-31 18:13:23 -0400

    Edited ages text

--- uncas/components/display/AgesText.tsx
+++ uncas/components/display/AgesText.tsx
@@ -4,7 +4,7 @@
 const AgesText = () => {
   return (
     <p className="w-[90%] text-sm sm:text-base md:text-lg font-serif font-semibold italic">
-      There were eight additional unnamed, enslaved childred who were transported to New Orleans on this shipment of people.  The total enslaved transported was 100, not 92.
+      There were eight additional unnamed, enslaved childred who were transported to New Orleans on this shipment of people.  The total number of enslaved transported was 100, not 92.
     </p>
   )
 }
