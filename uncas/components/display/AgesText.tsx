287587f84594419ef79a7775bacdc18265ec5a7e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-03-31 16:41:03 -0400

    Added Readme file and ages text component

---
+++ uncas/components/display/AgesText.tsx
@@ -0,0 +1,11 @@
+
+
+
+const AgesText = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis fuga delectus possimus, autem suscipit nulla doloremque maiores laudantium tenetur rerum totam a dicta aspernatur vitae obcaecati eligendi rem facilis sint?
+    </div>
+  )
+}
+export default AgesText;
