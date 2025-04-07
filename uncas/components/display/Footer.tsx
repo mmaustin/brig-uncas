11c0b8bb67d5f23e9e049713e42958ab1397d908
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-04-07 16:49:38 -0400

    Installed shadcn tooltip, applied to external links

--- uncas/components/display/Footer.tsx
+++ uncas/components/display/Footer.tsx
@@ -2,6 +2,12 @@ import { externalSources } from "@/constants/captiveArray";
 import Link from "next/link";
 import { FaCopyright } from "react-icons/fa";
 import { GiSail } from "react-icons/gi";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const Footer = () => {
 
@@ -11,7 +17,7 @@ const Footer = () => {
     <footer className="py-8 xl:py-12 text-white bg-green-800/80 lg:h-full flex justify-center items-center mt-10 mb-0">
       <div className="w-[90%] flex justify-between items-center">
         <div className="w-full flex flex-col gap-2 sm:gap-0 sm:flex-row sm:justify-between items-center">
-        {/* <div className="w-full flex flex-row justify-between items-center"> */}
+          {/* <div className="w-full flex flex-row justify-between items-center"> */}
           <div className="">
             <Link href={'/'} className="text-base text-yellow-300 font-extrabold font-serif uppercase text-white" >Brig Uncas</Link>
           </div>
@@ -25,9 +31,18 @@ const Footer = () => {
             {
               externalSources.map((address, i) => {
                 return (
-                  <Link key={i} href={address.path} target="_blank" >
-                    <GiSail className="text-xl hover:text-yellow-300"/>
-                  </Link>
+                  <TooltipProvider key={i}>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Link key={i} href={address.path} target="_blank" >
+                          <GiSail className="text-xl hover:text-yellow-300" />
+                        </Link>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Add to library</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
                 )
               })
             }
