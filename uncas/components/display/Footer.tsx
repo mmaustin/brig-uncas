179b7a7033ce474f7e0bd6dbd368f2438e83bdb1
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-04-22 17:59:19 -0400

    More adjustments to footer elements, deleted commented code

--- uncas/components/display/Footer.tsx
+++ uncas/components/display/Footer.tsx
@@ -1,13 +1,6 @@
 import { externalSources } from "@/constants/captiveArray";
 import Link from "next/link";
 import { FaCopyright } from "react-icons/fa";
-import { GiSail } from "react-icons/gi";
-// import {
-//   Tooltip,
-//   TooltipContent,
-//   TooltipProvider,
-//   TooltipTrigger,
-// } from "@/components/ui/tooltip";
 
 const Footer = () => {
 
@@ -17,7 +10,6 @@ const Footer = () => {
     <footer className="py-8 xl:py-12 text-white bg-green-800/80 lg:h-full flex justify-center items-center mt-10 mb-0">
       <div className="w-[90%] flex justify-between items-center">
         <div className="w-full flex flex-col gap-2 sm:gap-0 sm:flex-row sm:justify-between items-center">
-          {/* <div className="w-full flex flex-row justify-between items-center"> */}
           <div className="w-[200px] text-center">
             <Link href={'/'} className="text-base text-yellow-300 font-extrabold font-serif uppercase text-white" >Brig Uncas</Link>
           </div>
@@ -26,30 +18,15 @@ const Footer = () => {
             <p className="text-sm md:text-base text-white">{year}</p>
             <p className="text-sm md:text-base text-white">McCray Austin</p>
           </div>
-          {/* <nav className="flex flex-row justify-center items-end gap-2"> */}
-          <nav className="flex flex-row justify-center items-end gap-2 w-auto sm:w-[200px] md:w-auto border">
-            {/* <span className="text-xs font-serif text-yellow-300 uppercase">sources:</span> */}
+          <nav className="flex flex-row justify-center items-center sm:flex-col gap-3 sm:gap-1 w-auto sm:w-[200px]">
             {
               externalSources.map((address, i) => {
                 return (
-                  <figure key={i} className="border border-red-400">
+                  <figure key={i} className="m">
                     <Link key={i} href={address.path} target="_blank" >
-                      <GiSail className="text-xl hover:text-yellow-300" />
-                      <figcaption className="text-[9px] text-yellow-300">{address.source}</figcaption>
+                      <figcaption className="text-[10px] text-yellow-300">{address.source}</figcaption>
                     </Link>
                   </figure>
-                  // <TooltipProvider key={i}>
-                  //   <Tooltip>
-                  //     <TooltipTrigger asChild>
-                  //       <Link key={i} href={address.path} target="_blank" >
-                  //         <GiSail className="text-xl hover:text-yellow-300" />
-                  //       </Link>
-                  //     </TooltipTrigger>
-                  //     <TooltipContent side="left">
-                  //       <p>{address.source}</p>
-                  //     </TooltipContent>
-                  //   </Tooltip>
-                  // </TooltipProvider>
                 )
               })
             }
