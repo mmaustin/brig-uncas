9c5d92abd763adadd7377c3026203c66db58151c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-04-22 17:12:01 -0400

    Removed sources label from footer, added figcaption text to external icons

--- uncas/components/display/Footer.tsx
+++ uncas/components/display/Footer.tsx
@@ -18,22 +18,24 @@ const Footer = () => {
       <div className="w-[90%] flex justify-between items-center">
         <div className="w-full flex flex-col gap-2 sm:gap-0 sm:flex-row sm:justify-between items-center">
           {/* <div className="w-full flex flex-row justify-between items-center"> */}
-          <div className="">
+          <div className="border w-[200px]">
             <Link href={'/'} className="text-base text-yellow-300 font-extrabold font-serif uppercase text-white" >Brig Uncas</Link>
           </div>
-          <div className="w-[200px] flex flex-row justify-center items-center gap-2 ">
+          <div className="w-[200px] flex flex-row justify-center items-center gap-2 border">
             <FaCopyright className="rounded-lg w-4 h-4 text-white/70" />
             <p className="text-base text-white">{year}</p>
             <p className="text-base text-white">McCray Austin</p>
           </div>
-          <nav className="flex flex-row justify-center items-end gap-2">
-            <span className="text-xs font-serif text-yellow-300 uppercase">sources:</span>
+          {/* <nav className="flex flex-row justify-center items-end gap-2"> */}
+          <nav className="flex flex-row justify-center items-end gap-2 border w-[200px]">
+            {/* <span className="text-xs font-serif text-yellow-300 uppercase">sources:</span> */}
             {
               externalSources.map((address, i) => {
                 return (
                   <figure key={i} >
                     <Link key={i} href={address.path} target="_blank" >
                       <GiSail className="text-xl hover:text-yellow-300" />
+                      <figcaption className="text-[8px] ">{address.source}</figcaption>
                     </Link>
                   </figure>
                   // <TooltipProvider key={i}>
